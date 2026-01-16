36049f5d6b2d7b06dfaf527bf564a6e9119da808
Dorian Eydoux <deydoux@student.42lyon.fr>
2026-01-16 15:46:21 +0100

    Fix sub instruction in ft_strcmp

--- src/ft_strcmp.s
+++ src/ft_strcmp.s
@@ -7,17 +7,19 @@ ft_strcmp:
 	; (const char *s1)rsi
 
 	xor rax, rax ; rax = 0
+	xor rbx, rbx ; rbx = 0
 	jmp .start_loop ; goto .start_loop
 
 .loop:
 	inc rdi ; rdi++
 	inc rsi ; rsi++
 .start_loop:
-	mov al, byte [rsi] ; al = *rsi
-	sub al, byte [rdi] ; al -= *rdi
-	neg rax ; rax *= -1
+	mov al, byte [rdi] ; al = *rdi
+	mov bl, byte [rsi] ; bl = *rsi
+	sub rax, rbx ; al -= *rsi
+
 	test al, al
-	jnz .end ; if (bl != 0) goto .end
+	jnz .end ; if (al != 0) goto .end
 
 	cmp byte [rdi], 0
 	je .loop ; if (*rdi == 0) goto .loop
