9b1acbe94280c2927516d125a8c3384655430acd
Dorian Eydoux <deydoux@student.42lyon.fr>
2026-01-20 10:10:48 +0100

    Implement ft_read

--- include/libasm.h
+++ include/libasm.h
@@ -6,6 +6,7 @@
 char *ft_strcpy(char *dest, const char *src);
 int ft_strcmp(const char *s1, const char *s2);
 size_t ft_strlen(const char *s);
+ssize_t ft_read(int fd, void *buf, size_t count);
 ssize_t ft_write(int fd, const void *buf, size_t count);
 
 #endif /* _LIBASM_H_ */
---
+++ src/ft_read.s
@@ -0,0 +1,25 @@
+extern __errno_location
+
+section .text
+	global ft_read
+
+; ssize_t ft_read(int fd, void *buf, size_t count);
+ft_read:
+	; (int fd)edi
+	; (void *buf)rsi
+	; (size_t count)rdx
+
+	mov eax, 0 ; eax = 0 // read system call number
+	syscall ; write(edi, rsi, rdx)
+
+	cmp rax, 0
+	jge .end ; if (rax >= 0) goto .end
+
+	mov ebx, eax ; ebx = eax
+	neg ebx ; ebx *= -1
+	call __errno_location
+	mov [rax], ebx ; *rax = ebx
+	mov rax, -1 ; rax = -1
+
+.end:
+	ret ; return rax
